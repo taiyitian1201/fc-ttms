c31e3bd9046de31b012f7af39079a5827e450414
lee hom <102334679+leehom22@users.noreply.github.com>
2025-11-22 00:22:46 +0800

    shadcn components install

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,28 @@
-<script>
-import { Button } from './components/ui/button';
+<script setup>
+  import { Button } from './components/ui/button';
+  import Login from './pages/Login.vue';
+import { useUserStore } from './stores/user';
+  import "./style.css"
+  import Navbar from './components/navbar.vue';
+  import Footer from './components/Footer.vue';
+import { onMounted } from 'vue';
 
+  // const isAuth = localStorage.getItem("session_id_utm_ttms")
+  const user =  localStorage.getItem("session_id_utm_ttms") ? useUserStore() : null
+
+  onMounted(() => {
+    if (user && !user.isLoggedIn) {
+      // If there's a session ID in localStorage but the user store is not logged in, set the user as logged in
+      user.isLoggedIn = true;
+      // Optionally, you can fetch and set other user details here if needed
+    }
+  });
 </script>
 
 <template>
-  <Button class="text-red-500">Hello World</Button>
+  <Navbar v-if="user.isLoggedIn"/>
+  <router-view />
+  <Footer/>
 </template>
 
-<style scoped>
-
-
-</style>
+<style scoped></style>
